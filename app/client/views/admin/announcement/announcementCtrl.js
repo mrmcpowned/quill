2e3e44e799b3f3cf216019d57c0ded087d23edb5
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2018-08-18 02:28:02 -0400

    Is it because I didn't have an ID and proper converter?

--- app/client/views/admin/announcement/announcementCtrl.js
+++ app/client/views/admin/announcement/announcementCtrl.js
@@ -1,38 +1,18 @@
 angular.module('reg')
   .controller('AnnouncementCtrl', [
-    '$rootScope',
     '$scope',
+    '$sce',
     'settings',
-    'Utils',
-    'AuthService',
-    'Session',
-    'EVENT_INFO',
-    '$window',
-    function($rootScope, $scope, Settings, Utils, AuthService, Session, EVENT_INFO, $window){
+    function($scope, $sce, Settings){
 
-      var settings = Settings.data;
-      var user = $rootScope.currentUser;
+      var Settings = Settings.data;
 
-      $scope.EVENT_INFO = EVENT_INFO;
+      $scope.isAnnouncementEnabled = 
+        (Settings.announcementText == "" ||
+        Settings.announcementText == null,
+        Settings.announcementText == undefined) ? false : true;
 
-      $scope.pastConfirmation = Utils.isAfter(user.status.confirmBy);
-
-      $scope.logout = function(){
-        AuthService.logout();
-      };
-
-      $scope.showSidebar = false;
-      $scope.toggleSidebar = function(){
-        $scope.showSidebar = !$scope.showSidebar;
-      };
-
-      // oh god jQuery hack
-      $('.item').on('click', function(){
-        $scope.showSidebar = false;
-      });
-
-      $scope.goCrowdForge = function(){
-        $window.open('https://crowdforge.io/hackathons/shellhacks', '_blank');
-      };
-
+      var converter = new showdown.Converter();
+      $scope.announcementText = $sce.trustAsHtml(converter.makeHtml(Settings.announcementText));
+      
     }]);
